6e3c8fbfd2952d718a3f823fb919700d5c6def17
miquelmasso <miquelmassomoreno@gmail.com>
2021-04-23 20:27:38 +0200

    benne

--- src/refactoring/Lloguer.java
+++ src/refactoring/Lloguer.java
@@ -1,16 +1,43 @@
 package refactoring;
-
+ 
 import java.util.ArrayList;
-
+import java.util.Date;
+ 
 public class Lloguer {
-
-	public Lloguer(Date date, int i, Vehicle vehicleBasic) {
-		
-	}
-	private Date data;
-	private int dies;
-	private ArrayList<Vehicle> vehicles;
-	public Date getData() {
-		return null;
-	}
+        private Date data;
+        private int dies;
+        private Vehicle vehicle;
+       
+        public Lloguer(Date data, int dies,  Vehicle vehicle) {
+                super();
+                this.data = data;
+                this.dies = dies;
+                this.vehicle = vehicle;
+        }
+       
+       
+        public Date getData() { return data; }
+        public int getDies() { return dies;     }
+        public Vehicle getVehicle() { return vehicle; }
+   
+    public double quantitat() {
+        double quantitat = 0;
+        switch (this.getVehicle().getCategoria()) {
+            case Vehicle.BASIC:
+                quantitat += 3;
+                if (this.getDies() > 3) {
+                    quantitat += (this.getDies() - 3) * 1.5;
+                }
+            case Vehicle.GENERAL:
+                quantitat += 4;
+                if (this.dies > 2) {
+                    quantitat += (this.getDies() - 2) * 2.5;
+                }
+                break;
+            case Vehicle.LUXE:
+                quantitat += this.getDies() * 6;
+                break;
+        }
+                return quantitat;
+    }
 }
--- src/refactoring/Main.java
+++ src/refactoring/Main.java
@@ -1,20 +1,27 @@
 package refactoring;
+ 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-
+ 
 public class Main {
-
-	public static void main(String[] args)throws ParseException {
-		// demostració de construcció d'un vehicle de categoria BASIC
-		Vehicle vehicleBasic = new Vehicle("Tata", "Vista", Vehicle.BASIC);
-
-		// demostració de construccuó d'un lloguer amb una data
-		SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
-		Date date = dateFormat.parse("2/8/2013");
-		Lloguer lloguerBasic = new Lloguer(date, 2, vehicleBasic);
-
-		// demostració de formatat d'una data
-		System.out.println(dateFormat.format(lloguerBasic.getData()));
-
-	}
-
+ 
+        public static void main(String[] args) throws ParseException {
+               
+                // demostració de construcció d'un vehicle de categoria BASIC
+                Vehicle vehicleBasic = new Vehicle("Tata", "Vista", Vehicle.BASIC);
+ 
+                // demostració de construccuó d'un lloguer amb una data
+                SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
+                Date date = dateFormat.parse("2/8/2013");
+                Lloguer lloguerBasic = new Lloguer(date, 2, vehicleBasic);
+ 
+                // demostració de formatat d'una data
+                System.out.println(dateFormat.format(lloguerBasic.getData()));
+        }
+        public static String GestorLloguersLite(Client c){
+        return c.informe();
+    }
+ 
 }
+ 
